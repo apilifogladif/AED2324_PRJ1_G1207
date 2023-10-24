b963d3ab653877db12becac85bef93a8b294d1fc
apilifogladif <filipajfidalgo@gmail.com>
2023-10-24 21:50:45 +0100

    Constructors

---
+++ Class.h
@@ -0,0 +1,12 @@
+#ifndef __Class_h__
+#define __Class_h__
+
+#include <string>
+
+namespace using std;
+
+class Class {
+    Class(string classCode, string UcCode, string Weekday, double startHour, double , string type);
+};
+
+#endif
---
+++ Student.h
@@ -0,0 +1,12 @@
+#ifndef __Student_h__
+#define __Student_h__
+
+#include <string>
+
+namespace using std;
+
+class Student {
+    Student(string StudentCode, string StudentName, string UcCode, string ClassCode);
+};
+
+#endif
